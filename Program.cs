46bedb7fb22e36c3614364f4959b8995d9cbac30
Maryna Horshevska <69041379+MarinaGorsh@users.noreply.github.com>
2024-03-17 13:49:24 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
-
+using _6lr;
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ICompaniesService, CompaniesService>();//об'єкт сервісу створюється при першому зверненні до нього, всі наступні запити використовують той самий раніше створений об'єкт сервісу
+builder.Services.AddSingleton<IPhonesService, PhonesService>();//об'єкт сервісу створюється при першому зверненні до нього, всі наступні запити використовують той самий раніше створений об'єкт сервісу
+builder.Services.AddSingleton<IUsersService, UsersService>();//об'єкт сервісу створюється при першому зверненні до нього, всі наступні запити використовують той самий раніше створений об'єкт сервісу
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "lr1");
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "lr7");
     });
 }
 
