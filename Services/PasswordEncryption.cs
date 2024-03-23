536e352c3ff1b4b91b12c18c3d65638abf979c1b
Maryna Horshevska <69041379+MarinaGorsh@users.noreply.github.com>
2024-03-23 19:50:56 +0100

    Add files via upload

---
+++ Services/PasswordEncryption.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _6lr
+{
+    public class PasswordEncryptionService
+    {
+
+        public string EncryptPassword(string password)
+        {
+            string encryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(password);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    password = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return password;
+        }
+    }
+}
