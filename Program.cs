9a63b4df6e94b5c34c65062fd4557050d9e6fa7b
Maryna Horshevska <69041379+MarinaGorsh@users.noreply.github.com>
2024-03-23 19:42:18 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,59 @@
 using _6lr;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ICompaniesService, CompaniesService>();//об'єкт сервісу створюється при першому зверненні до нього, всі наступні запити використовують той самий раніше створений об'єкт сервісу
-builder.Services.AddSingleton<IPhonesService, PhonesService>();//об'єкт сервісу створюється при першому зверненні до нього, всі наступні запити використовують той самий раніше створений об'єкт сервісу
-builder.Services.AddSingleton<IUsersService, UsersService>();//об'єкт сервісу створюється при першому зверненні до нього, всі наступні запити використовують той самий раніше створений об'єкт сервісу
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "lr8", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
 
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+builder.Services.AddSingleton<ICompaniesService, CompaniesService>();
+builder.Services.AddSingleton<IPhonesService, PhonesService>();
+builder.Services.AddSingleton<IUsersService, UsersService>();
+
+builder.Services.AddAuthentication( JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true
+        };
+    });
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -15,13 +61,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "lr7");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "lr8");
     });
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
