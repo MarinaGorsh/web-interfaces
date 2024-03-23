198da6712f28f97a04240c31e66166e59ec35a53
Maryna Horshevska <69041379+MarinaGorsh@users.noreply.github.com>
2024-03-23 19:48:25 +0100

    Add files via upload

---
+++ Models/AuthUsers.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace _6lr
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
