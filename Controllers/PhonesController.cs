89bbb947641f8bcb388d750dd397f5eeb0b9d231
Maryna Horshevska <69041379+MarinaGorsh@users.noreply.github.com>
2024-03-23 19:46:12 +0100

    Update PhonesController.cs(add Authorize)

--- Controllers/PhonesController.cs
+++ Controllers/PhonesController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _6lr.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class PhonesController : ControllerBase{
