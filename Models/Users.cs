422d0e41d58246557af44e19e390e383c98074f4
Maryna Horshevska <69041379+MarinaGorsh@users.noreply.github.com>
2024-03-23 19:47:43 +0100

    Change structure

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,11 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace _6lr
 {
     public class Users
     {
         public int Id { get; set; }
+        [MaxLength(15, ErrorMessage = "Ім'я не повинно перевищувани 15 букв, але якщо ваше ім'я містить більше, не вибачаюсь")]
         public string? Name { get; set; }
+        [MaxLength(15, ErrorMessage = "Прізвище не повинно перевищувани 15 букв, але якщо ваше мрізвище містить більше, не вибачаюсь")]
         public string? Surname { get; set; }
-        public string? PhoneBrand { get; set; }
-        public string? PhoneName { get; set; }
+        [EmailAddress(ErrorMessage = "Некоректна адреса")]
+        public string? Email { get; set; }
+        public DateTime Birth { get; set; }
+        public string? Password { get; set; }
+        public DateTime LastAuth { get; set; }
+        public int ErrAuth { get; set; }
+
     }
 }
